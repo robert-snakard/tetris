caf1f01a735b2b633ad3e7a9addc57f713dac4e8
Robert Snakard <robert.snakard@gmail.com>
2019-09-17 15:38:38 -0700

    Tetris video framework.
    The first iteration of the tetris video framework. Draws the board,
    the individual pieces, and does so within the game loop.
    
    App is purposefully pub in Game. I don't want any App processing done
    in Game and adding a setter/getter will encourage that.
    
    EventQueue is included in this commit so WebApp isn't a useless
    wrapper function. For benchmarking purposes.

---
+++ src/game.rs
@@ -0,0 +1,77 @@
+use crate::app::WebApp;
+use crate::pieces::*;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use wasm_bindgen::prelude::*;
+
+pub struct Game {
+pub app: WebApp,
+    board: [[Option<u8>; 10]; 20],
+    cur_piece: usize,
+    total_time: f64,
+}
+
+impl Game {
+    pub fn new(hook_id: &str) -> Game {
+        let app = WebApp::new(hook_id);
+        let board = [[None; 10]; 20];
+
+        Game { 
+            app, 
+            board, 
+            cur_piece: 0,
+            total_time: 0.0,
+        }
+    }
+
+    pub fn run(&mut self, etime: f64) {
+        self.total_time += etime;
+
+        if self.total_time > 1000.0 {
+            self.draw_board();
+            self.draw_piece(3, 8, self.cur_piece, 0);
+            self.cur_piece = (self.cur_piece + 1) % 7;
+            self.total_time = 0.0;
+        }
+    }
+
+    fn draw_board(&mut self) -> Result<(), JsValue> {
+        for y in 0..20 {
+            for x in 0..10 {
+                if let Some(_) = self.board[y][x] {
+                    self.app.ctx.set_fill_style(&JsValue::from("green"));
+                } else {
+                    self.app.ctx.set_fill_style(&JsValue::from("black"));
+                }
+
+                self.app.ctx.fill_rect((x as f64) * 10.0, (y as f64) * 10.0, 10.0, 10.0);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn draw_piece(&mut self, xcoord: u8, ycoord: u8, piece: usize, rotation: usize) -> Result<(), JsValue> {
+        let p = PIECES[piece][rotation];
+        self.app.ctx.set_fill_style(&JsValue::from(format!("hsl({}, 100%, 50%", piece*45)));
+
+        // Special case yellow b/c people's green cones are too powerful!
+        if piece == 2 {
+            self.app.ctx.set_fill_style(&JsValue::from("hsl(60, 100%, 50%"));
+        }
+
+        for y in 0..4 {
+            for x in 0..4 {
+                if p[(y*4 + x) as usize] == 1 {
+                    self.app.ctx.fill_rect((xcoord+x) as f64 * 10.0,
+                                           (ycoord+y) as f64 * 10.0,
+                                           10.0, 10.0);
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,43 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod app;
+mod events;
+mod game;
+mod pieces;
+
+use crate::game::Game;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use wasm_bindgen::prelude::*;
+
+const WIDTH: u32 = 100;
+const HEIGHT: u32 = 100;
+
+const HOOK_ID: &str = "tetris";
+
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    let mut game = Rc::new(RefCell::new(Game::new(HOOK_ID)));
+
+    pub fn run(game: Rc<RefCell<Game>>) -> Result<(), JsValue> {
+        let mut old_time = 0.0;
+        let mut etime = 0.0;
+
+        let g2 = game.clone();
+        let lp1 = Rc::new(RefCell::new(None));
+        let lp2 = lp1.clone();
+
+        *lp2.borrow_mut() = Some(Closure::wrap(Box::new(move |new_time: f64| {
+            etime = new_time - old_time;
+            old_time = new_time;
+            game.borrow_mut().run(etime);
+
+            game.borrow_mut().app.request_animation_frame(lp1.borrow().as_ref().unwrap());
+        }) as Box<dyn FnMut(f64)>));
+        g2.borrow_mut().app.request_animation_frame(lp2.borrow().as_ref().unwrap());
+       
+        Ok(())
+    } run(game.clone());
+
+    Ok(())
 }
