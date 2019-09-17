5319b1d057662466ab796914b1f3bd0bea905c0b
Robert Snakard <robert.snakard@gmail.com>
2019-09-17 15:38:38 -0700

    get wasm working

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use wasm_bindgen::prelude::*;
+use web_sys::*;
+
+#[wasm_bindgen]
+pub fn start_game() {
+    window().unwrap().alert().unwrap();
 }
